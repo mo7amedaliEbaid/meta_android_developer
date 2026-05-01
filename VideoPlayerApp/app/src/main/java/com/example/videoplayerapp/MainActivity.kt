a4f3099263a7964452a48c1120d741cddafa1f86
mo7amedaliEbaid <mo7amedaliebaid@gmail.com>
2026-05-01 18:54:06 +0300

    Initial commit

---
+++ VideoPlayerApp/app/src/main/java/com/example/videoplayerapp/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.videoplayerapp
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.videoplayerapp.databinding.ActivityMainBinding
+
+
+import android.net.Uri
+import android.net.Uri.*
+import android.widget.MediaController
+import android.widget.VideoView
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        //Find the VideoView class by its id
+        val videoView = findViewById<VideoView>(binding.testView.id)
+        //Creating MediaController
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+        //specify the location of media file
+        val uri:Uri = parse(
+            "android.resource://" + packageName
+                    + "/raw/test"
+        )
+        //Setting MediaController and URI, then starting the videoView
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.start()
+    }
+}
