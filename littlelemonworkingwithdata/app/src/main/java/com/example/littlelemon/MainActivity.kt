9c4e27abd5ff4fccd10333faf0dc003cd9d1badf
mo7amedaliEbaid <mo7amedaliebaid@gmail.com>
2026-05-09 14:18:23 +0300

    Add initial project structure and implement network data models
    - Created MenuNetwork and MenuItemNetwork data classes for network data handling.
    - Added UI theme files including Color, Shape, Theme, and Type for consistent styling.
    - Introduced drawable resources for app icons and backgrounds.
    - Set up Gradle build files and properties for project configuration.
    - Added XML files for backup and data extraction rules.
    - Implemented example unit test for basic functionality.
    - Established settings.gradle for project management and dependencies.

---
+++ littlelemonworkingwithdata/app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -0,0 +1,143 @@
+package com.example.littlelemon
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
+import androidx.room.Room
+import com.example.littlelemon.ui.theme.LittleLemonTheme
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.http.ContentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    private val httpClient = HttpClient(Android) {
+        install(ContentNegotiation) {
+            json(contentType = ContentType("text", "plain"))
+        }
+    }
+
+    private val database by lazy {
+        Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database").build()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LittleLemonTheme {
+                val databaseMenuItems by database.menuItemDao().getAll().observeAsState(emptyList())
+
+                var orderMenuItems by remember { mutableStateOf(false) }
+
+                var searchPhrase by remember { mutableStateOf("") }
+
+                val sortedItems = if (orderMenuItems) {
+                    databaseMenuItems.sortedBy { it.title }
+                } else {
+                    databaseMenuItems
+                }
+
+                val menuItems = if (searchPhrase.isNotEmpty()) {
+                    sortedItems.filter { it.title.contains(searchPhrase, ignoreCase = true) }
+                } else {
+                    sortedItems
+                }
+
+                Column(modifier = Modifier.padding(16.dp)) {
+                    androidx.compose.foundation.Image(
+                        painter = painterResource(id = R.drawable.logo),
+                        contentDescription = "logo",
+                        modifier = Modifier.padding(50.dp)
+                    )
+
+                    Button(
+                        onClick = { orderMenuItems = true },
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(text = "Tap to Order By Name")
+                    }
+
+                    OutlinedTextField(
+                        value = searchPhrase,
+                        onValueChange = { newValue -> searchPhrase = newValue },
+                        label = { Text("Search") },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 50.dp, end = 50.dp)
+                    )
+
+                    MenuItemsList(items = menuItems)
+                }
+            }
+        }
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            if (database.menuItemDao().isEmpty()) {
+                val menuItems = fetchMenu()
+                saveMenuToDatabase(menuItems)
+            }
+        }
+    }
+
+    private suspend fun fetchMenu(): List<MenuItemNetwork> {
+        val response = httpClient.get("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/littleLemonSimpleMenu.json")
+        return response.body<MenuNetwork>().menu
+    }
+
+    private fun saveMenuToDatabase(menuItemsNetwork: List<MenuItemNetwork>) {
+        val menuItemsRoom = menuItemsNetwork.map { it.toMenuItemRoom() }
+        database.menuItemDao().insertAll(*menuItemsRoom.toTypedArray())
+    }
+}
+
+@Composable
+private fun MenuItemsList(items: List<MenuItemRoom>) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(top = 20.dp)
+    ) {
+        items(items) { menuItem ->
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(text = menuItem.title)
+                Text(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(5.dp),
+                    textAlign = TextAlign.Right,
+                    text = "%.2f".format(menuItem.price)
+                )
+            }
+        }
+    }
+}
